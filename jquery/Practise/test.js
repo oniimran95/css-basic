a77f03cd58b6423ed6378f620fcab4129c31b655
oniimran95 <oni.imran95@gmail.com>
2023-10-17 19:03:30 +0600

    ajex

---
+++ jquery/Practise/test.js
@@ -0,0 +1,17 @@
+// function sleep(name, time) {
+//     console.log(name + " is sleeping from "+ time)
+// }
+
+// sleep("Jashim", "10pm")
+// sleep("Rahim", " 11pm")
+// sleep("Karim", " 12pm")
+
+
+var carName="BMW";
+function myfuntion() {
+    var carName = "Volvo";
+    console.log(carName);
+}
+
+myfuntion()
+console.log(carName)
